2ea96fb551192381c166b84e7fa79bf39e2654c4
K_Arai00 <attckonk@gmail.com>
2020-09-16 17:51:31 +0900

    プレイヤーの腕を作り、タッチするときのアニメーションを作りました。

---
+++ Assets/kakuji Folder/Arai_kanta/Scripts/TouchAnim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchAnim : MonoBehaviour
+{
+    public Animation anim;
+    void Update()
+    {
+        //左クリックを受け付ける
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim = this.gameObject.GetComponent<Animation>();
+            anim.Play();
+            Debug.Log("Left Click.");
+        }
+    }
+}
