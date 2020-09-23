1ad5a862428f21f045c2f80f10e038a58e769773
K_Arai00 <attckonk@gmail.com>
2020-09-23 16:22:59 +0900

    この前追加したプレイヤーのタッチアニメーションのパラメーターを同期させました。 自分の操作キャラだけがアニメーションするようにしました。

--- Assets/kakuji Folder/Arai_kanta/Scripts/TouchAnim.cs
+++ Assets/kakuji Folder/Arai_kanta/Scripts/TouchAnim.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class TouchAnim : MonoBehaviour
 {
-    public Animation anim;
+    PhotonView m_photonView;
+    private Animator anim;
+    private void Start()
+    {
+        m_photonView = GetComponent<PhotonView>();
+    }
     void Update()
+    {
+        if (m_photonView.IsMine) 
+        {
+            Anim();
+        } 
+    }
+
+    private void Anim() 
     {
         //左クリックを受け付ける
         if (Input.GetMouseButtonDown(0))
         {
-            anim = this.gameObject.GetComponent<Animation>();
-            anim.Play();
+            anim = this.gameObject.GetComponent<Animator>();
+            anim.SetTrigger("Touch");
             Debug.Log("Left Click.");
         }
     }
